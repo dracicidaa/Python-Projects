240b7e95c3027116ab288d31d3310e635128bd51
dracicidaa <michael@englesby.cc>
2026-04-30 23:36:34 -0600

    Modified main.py to allow for an image of a particular day to be pulled and displayed via pygame. There seems to be a bit of a bug where the window wont close when the x is it too quickly. I also had to figure out how to securely make use of an API key within the program, I nearly published it with my personal NASA API key.

--- Nasa-IOTD-Display/main.py
+++ Nasa-IOTD-Display/main.py
@@ -2,14 +2,35 @@
 #Version 0.1
 #Change log:
 #-created window with pygame
+#-added nasapy pull request for images using demo key
+#-displayed an image via pygame
+#-used IO to store an image
 
 
 #Import dependencies
 import os
+import nasapy
 import pygame
+import requests
+import io
+from dotenv import load_dotenv
 
 #Initialize stuff
 pygame.init()
+load_dotenv()
+apiKey = os.getenv('NASA_KEY')
+nasa = nasapy.Nasa(apiKey)
+
+#Functions
+def nasaPicture():
+    #Get nasa image of the day
+    apod = nasa.picture_of_the_day(date='2000-04-28')
+    imgUrl = apod['url']
+    #download the photo into ram
+    response = requests.get(imgUrl)
+    imageBits = io.BytesIO(response.content)
+    return imageBits
+
 
 #Initialize the screen, 
 screen = pygame.display.set_mode((800, 600))
@@ -19,14 +40,22 @@ pygame.display.set_caption('NASA Image Of The Day V0.1')
 running = True
 while running:
     #check for events
+    print('check events')
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
     
     #fill background with a color
+    print('screen fill')
     screen.fill((155, 200, 10))
+    nasaAPOD = pygame.image.load(nasaPicture()).convert()
+
+    #blit the photo of the day to the screen
+    print('screen blit')
+    screen.blit(nasaAPOD, (0,0))
 
     #update the display
+    print('screen flip')
     pygame.display.flip()
 
 #cleanly exit pygame
