eb13b75d0eb935689c831ea39d54cf9704fc16d1
dracicidaa <michael@englesby.cc>
2026-05-01 00:15:26 -0600

    Added a resize to the window, am struggling to get it to grab and store the image.

--- Nasa-IOTD-Display/main.py
+++ Nasa-IOTD-Display/main.py
@@ -29,13 +29,26 @@ def nasaPicture():
     #download the photo into ram
     response = requests.get(imgUrl)
     imageBits = io.BytesIO(response.content)
+    imageBits.seek(0)
     return imageBits
+
+def nasaSurface(imageData):
+    #get needed image, rects, centers
+    nasaAPOD = pygame.image.load(imageData).convert()
+    screenRect = screen.get_rect()
+    imageRect = nasaAPOD.get_rect()
+    imageRect.center = screenRect.center
+    #blit the photo of the day to the screen
+    print('screen blit')
+    screen.blit(nasaAPOD, imageRect)
 
 
 #Initialize the screen, 
-screen = pygame.display.set_mode((800, 600))
+screen = pygame.display.set_mode((800, 600), pygame.RESIZABLE)
 pygame.display.set_caption('NASA Image Of The Day V0.1')
 
+#Fetch the daily image to initialize
+imageData = pygame.image.load(nasaSurface(nasaPicture()))
 #Main loop
 running = True
 while running:
@@ -44,16 +57,16 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        
+        if event.type == pygame.VIDEORESIZE:
+            #update screen size
+            screen = pygame.display.set_mode((event.w, event.h), pygame.RESIZABLE)
     
     #fill background with a color
     print('screen fill')
     screen.fill((155, 200, 10))
-    nasaAPOD = pygame.image.load(nasaPicture()).convert()
-
-    #blit the photo of the day to the screen
-    print('screen blit')
-    screen.blit(nasaAPOD, (0,0))
-
+    nasaSurface(imageData)
+    
     #update the display
     print('screen flip')
     pygame.display.flip()
